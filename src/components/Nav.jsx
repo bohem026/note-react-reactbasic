bec36592ea997fe6dc9b0a2c577e164da85b6294
bohem dev <microbin98@gmail.com>
2026-06-23 14:16:29 +0900

    feat: print list

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,10 @@
 //Nav 컴포넌트
-function Nav() {
-  const lists = [];
+function Nav({ data }) {
+  const lists = data.map((d) => (
+    <li key={d.id}>
+      <a href="">{d.title}</a>
+    </li>
+  ));
   // list에 출력할 코드 생성
 
   return (
