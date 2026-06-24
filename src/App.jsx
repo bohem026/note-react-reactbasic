fa5ffb39385d5002d605d80d2c57277675d099ec
bohem dev <microbin98@gmail.com>
2026-06-24 16:42:49 +0900

    feat: diff 필드 추가 및 관련 컴포넌트 수정

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,14 @@ function App() {
     desc: '기본언어인 html, css, javascript부터 학습합니다.',
   });
   const [content, setContent] = useState([
-    { id: '1', title: 'UI/UX 개발', desc: '사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현' },
+    { id: '1', title: 'UI/UX 개발', desc: '사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현', diff: 5 },
     {
       id: '2',
       title: '재사용이 가능한 UI 개발',
       desc: '컴포넌트 기반으로 동일한 UI를 효율적으로 재사용 가능',
+      diff: 6,
     },
-    { id: '3', title: '애니메이션 구현', desc: '상태 변화에 따른 자연스럽고 동적인 화면 효과 구현' },
+    { id: '3', title: '애니메이션 구현', desc: '상태 변화에 따른 자연스럽고 동적인 화면 효과 구현', diff: 7 },
   ]);
   // const [maxId, setMaxid] = useState(3);
 
@@ -31,6 +32,7 @@ function App() {
 
   let _title = null;
   let _desc = null;
+  let _diff = null;
   let _article = null;
 
   const handleDelete = () => {
@@ -52,11 +54,13 @@ function App() {
     if (selected) {
       _title = selected.title;
       _desc = selected.desc;
+      _diff = selected.diff;
     }
     _article = (
       <MyArticle
         title={_title}
         desc={_desc}
+        diff={_diff}
         onChangeMode={() => {
           setMode('update');
         }}
@@ -66,10 +70,10 @@ function App() {
   } else if (mode === 'create') {
     _article = (
       <CreateArticle
-        onSubmit={(_title, _desc) => {
+        onSubmit={(_title, _desc, _diff) => {
           const newId = uuidv4();
 
-          let _contents = content.concat({ id: newId, title: _title, desc: _desc });
+          let _contents = content.concat({ id: newId, title: _title, desc: _desc, diff: _diff });
           setContent(_contents);
           // setMaxid(newId);
           setId(newId);
@@ -85,7 +89,8 @@ function App() {
       <UpdateArticle
         title={selected.title}
         desc={selected.desc}
-        onSubmit={(_title, _desc) => {
+        diff={selected.diff}
+        onSubmit={(_title, _desc, _diff) => {
           setContent((prev) =>
             prev.map((p) =>
               p.id === id
@@ -93,6 +98,7 @@ function App() {
                     ...p,
                     title: _title,
                     desc: _desc,
+                    diff: _diff,
                   }
                 : p,
             ),
