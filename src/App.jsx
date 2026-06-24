3a8a4088a8de9908a16840c1043ed8ab9a1ba4ef
bohem dev <microbin98@gmail.com>
2026-06-24 16:10:45 +0900

    feat: uuid 패키지 추가 및 ID 관리 방식 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { useState, useCallback } from 'react';
 import Controls from './components/controls';
 import CreateArticle from './components/createArticle';
 import UpdateArticle from './components/UpdateArticle';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   console.log('App render');
@@ -16,15 +17,15 @@ function App() {
     desc: '기본언어인 html, css, javascript부터 학습합니다.',
   });
   const [content, setContent] = useState([
-    { id: 1, title: 'UI/UX 개발', desc: '사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현' },
+    { id: '1', title: 'UI/UX 개발', desc: '사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현' },
     {
-      id: 2,
+      id: '2',
       title: '재사용이 가능한 UI 개발',
       desc: '컴포넌트 기반으로 동일한 UI를 효율적으로 재사용 가능',
     },
-    { id: 3, title: '애니메이션 구현', desc: '상태 변화에 따른 자연스럽고 동적인 화면 효과 구현' },
+    { id: '3', title: '애니메이션 구현', desc: '상태 변화에 따른 자연스럽고 동적인 화면 효과 구현' },
   ]);
-  const [maxId, setMaxid] = useState(3);
+  // const [maxId, setMaxid] = useState(3);
 
   const welcome = { title: 'welcome', desc: 'Welcome to react' };
 
@@ -66,11 +67,11 @@ function App() {
     _article = (
       <CreateArticle
         onSubmit={(_title, _desc) => {
-          const newId = maxId + 1;
+          const newId = uuidv4();
 
           let _contents = content.concat({ id: newId, title: _title, desc: _desc });
           setContent(_contents);
-          setMaxid(newId);
+          // setMaxid(newId);
           setId(newId);
           setMode('read');
         }}
@@ -103,6 +104,8 @@ function App() {
   }
 
   const handleChangeMode = useCallback((_id) => {
+    console.log(_id);
+
     setMode('read');
     setId(_id);
   }, []);
