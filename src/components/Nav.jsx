6f9f9d37bdb5338110b87d7932cad86e71247d6f
bohem dev <microbin98@gmail.com>
2026-06-24 11:19:51 +0900

    feat: memo added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,15 @@
 //Nav 컴포넌트
-function Nav({ data, onChangeMode }) {
-  console.log('Nav render');
+import { memo } from 'react';
 
+const Nav = memo(function Nav({ data, onChangeMode }) {
+  console.log('Nav render');
   const lists = data.map((d) => (
     <li key={d.id}>
       <a
         href={`/${d.id}`}
         data-id={d.id}
         onClick={(e) => {
-          console.log(e.target.dataset.id);
-
+          // console.log(e.target.dataset.id);
           e.preventDefault();
           onChangeMode(Number(e.target.dataset.id));
         }}
@@ -18,12 +18,12 @@ function Nav({ data, onChangeMode }) {
       </a>
     </li>
   ));
-  // list에 출력할 코드 생성
 
+  //list에 출력할 코드 생성
   return (
     <nav>
       <ul>{lists}</ul>
     </nav>
   );
-}
+});
 export default Nav;
